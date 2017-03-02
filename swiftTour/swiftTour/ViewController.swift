c7ed9c70b27000d55705438b626d1c7194b632d9
ixxcome <ixxcome@ixxcomes-macbook-pro.local>
2017-03-02 09:55:20 +0800

    add.

--- swiftTour/swiftTour/ViewController.swift
+++ swiftTour/swiftTour/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let btn = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        btn.backgroundColor = UIColor.red
+        view.addSubview(btn)
     }
 
 
